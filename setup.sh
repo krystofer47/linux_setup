31b2e4c1c0baa1515974cc115cd7c3777e080459
krystofer47 <christoph.brosch@outlook.de>
2020-02-24 11:30:55 +0100

    added remove question

--- setup.sh
+++ setup.sh
@@ -3,8 +3,15 @@
 [[ -d ~/bin ]] || mkdir ~/bin
 if [[ $EUID -eq 0 ]]; then
 	echo "This script is running with root access";
-	exit 0;
 else
 	echo "This script is not running with root access";
-	exit 1;
 fi
+
+# Done
+while read -p "Delete this folder[y|n]: " answer; do
+	[[ "$answer" = "n" ]] && break || [[ "$answer" = "y" ]] && echo "removing..." && break
+	echo "Not a valid answer" >&2
+done
+
+exit 0
+
